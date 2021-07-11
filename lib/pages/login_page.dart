17bf09dcdf85178b9689c3ac6e4f4e8a6826c4c7
dimaswiranata-diaconmed <dimas.wiranata@diaconmed.com>
2021-07-12 01:04:04 +0700

    add facebook login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 import 'package:gardana/scoped-models/main.dart';
 import 'package:gardana/widget/ui_element/custom_button.dart';
@@ -15,10 +16,45 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final googleSignIn = GoogleSignIn();
+  final _googleSignIn = GoogleSignIn();
 
-  void _login() async {
-    final user = await googleSignIn.signIn();
+  Future<void> _loginFacebook() async {
+    try {
+      final AccessToken result = await FacebookAuth.instance.login();
+      final FacebookAuthCredential facebookAuthCredential =
+          FacebookAuthProvider.credential(result.token);
+
+      final userFromFB = (await FirebaseAuth.instance
+              .signInWithCredential(facebookAuthCredential))
+          .user;
+      print(userFromFB);
+      final userData = await FacebookAuth.instance.getUserData();
+      // final userData = await FacebookAuth.instance.getUserData(fields: "email,birthday,friends,gender,link");
+      print(userData);
+    } on FacebookAuthException catch (e) {
+      // if the facebook login fails
+      print(e.message); // print the error message in console
+      // check the error type
+      switch (e.errorCode) {
+        case FacebookAuthErrorCode.OPERATION_IN_PROGRESS:
+          print("You have a previous login operation in progress");
+          break;
+        case FacebookAuthErrorCode.CANCELLED:
+          print("login cancelled");
+          break;
+        case FacebookAuthErrorCode.FAILED:
+          print("login failed");
+          break;
+      }
+    } catch (e, s) {
+      // print in the logs the unknown errors
+      print(e);
+      print(s);
+    }
+  }
+
+  void _loginGoogle() async {
+    final user = await _googleSignIn.signIn();
     if (user == null) {
       return;
     } else {
@@ -47,15 +83,13 @@ class _LoginPageState extends State<LoginPage> {
             LeanButton(
               child: Text('Masuk Dengan Google'),
               color: Colors.red,
-              onPressed: _login,
+              onPressed: _loginGoogle,
             ),
             SizedBox(height: 16),
             LeanButton(
               child: Text('Masuk Dengan Facebook'),
               color: Colors.blue,
-              onPressed: () {
-                print('masuk dengan facebook');
-              },
+              onPressed: _loginFacebook,
             )
           ]),
     ]);
